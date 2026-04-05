1c1c346c256fc2722d352fb67fee654ab55a83ae
Vit <vitalystark@gmail.com>
2026-04-05 18:22:20 +0200

    почистил репозиторий от лишнего

--- calculate.h
+++
@@ -1,7 +0,0 @@
-#ifndef CALCULATE_H_
-#define CALCULATE_H_
-
-void CALCULATE_find_all_intersections(int show_iterations);
-void CALCULATE_compute_area(void);
-
-#endif /*CALCULATE_H_*/
--- test.h
+++
@@ -1,6 +0,0 @@
-#ifndef TEST_H_
-#define TEST_H_
-
-void RUN_run_tests(void);
-
-#endif /*TEST_H_*/
